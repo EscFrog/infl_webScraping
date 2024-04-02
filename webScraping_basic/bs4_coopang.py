add94a3e5a44cadef66b56d6db3c2a7973f770fe
Walter Park <neodust223@gmail.com>
2024-04-02 22:30:56 +0900

    Create bs4_coopang.py
    쿠팡 스크래핑 연습중

---
+++ webScraping_basic/bs4_coopang.py
@@ -0,0 +1,11 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://www.coupang.com/np/search?q=%EB%85%B8%ED%8A%B8%EB%B6%81&channel=recent"
+headers = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36", "Accept-Language": "ko-KR,ko;q=0.8,en-US;q=0.5,en;q=0.3"}
+res = requests.get(url, headers=headers)
+res.raise_for_status()
+print(res.status_code)
+soup = BeautifulSoup(res.text, "lxml")
+
+print(res.text)
