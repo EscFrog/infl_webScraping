1ab1afa87a3ab9acd87e28fa0b31a05278f276d8
Walter Park <neodust223@gmail.com>
2024-04-03 19:00:55 +0900

    Update bs4_coopang.py

--- webScraping_basic/bs4_coopang.py
+++ webScraping_basic/bs4_coopang.py
@@ -1,4 +1,5 @@
 import requests
+import re
 from bs4 import BeautifulSoup
 
 url = "https://www.coupang.com/np/search?q=%EB%85%B8%ED%8A%B8%EB%B6%81&channel=recent"
@@ -8,4 +9,22 @@ res.raise_for_status()
 print(res.status_code)
 soup = BeautifulSoup(res.text, "lxml")
 
-print(res.text)
+items =  soup.find_all("li", attrs={"class": re.compile("^search-product")})
+
+for idx, item in enumerate(items):
+    name = item.find("div", attrs={"class": "name"}).get_text() # 제품명
+    price = item.find("strong", attrs={"class": "price-value"}).get_text() # 가격
+    
+    rate = item.find("em", attrs={"class": "rating"})
+    if rate:
+        rate = rate.get_text() # 평점
+    else:
+        rate = "평점 없음"
+    
+    rate_count = item.find("span", attrs={"class": "rating-total-count"}) # 평점수
+    if rate_count:
+        rate_count = rate_count.get_text() # 평점
+    else:
+        rate_count = "평점 없음"
+    
+    print(idx, "----> ", name.strip(), price.strip(), rate.strip(), rate_count.strip())
