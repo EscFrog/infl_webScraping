6021ed729efaf1b235f613b809ae3b4b2c8a7ec3
Walter Park <neodust223@gmail.com>
2024-04-04 11:23:06 +0900

    Update bs4_coopang.py

--- webScraping_basic/bs4_coopang.py
+++ webScraping_basic/bs4_coopang.py
@@ -12,19 +12,37 @@ soup = BeautifulSoup(res.text, "lxml")
 items =  soup.find_all("li", attrs={"class": re.compile("^search-product")})
 
 for idx, item in enumerate(items):
+    
+    # 광고 상품은 제외
+    ad_badge = item.find("span", attrs={"class":"ad-badge-text"})
+    if ad_badge:
+        print(" <광고 상품은 제외합니다.>")
+        continue
+    
     name = item.find("div", attrs={"class": "name"}).get_text() # 제품명
+    
+    # 애플 제품 제외
+    if "Apple" in name:
+        print(" <Apple 제품은 제외합니다.>")
+        continue
+    
     price = item.find("strong", attrs={"class": "price-value"}).get_text() # 가격
     
     rate = item.find("em", attrs={"class": "rating"})
     if rate:
         rate = rate.get_text() # 평점
     else:
-        rate = "평점 없음"
+        print(" <평점 없는 상품 제외합니다.>")
+        continue
     
     rate_count = item.find("span", attrs={"class": "rating-total-count"}) # 평점수
     if rate_count:
         rate_count = rate_count.get_text() # 평점
+        rate_count = rate_count[1:-1]
     else:
-        rate_count = "평점 없음"
+        print(" <평점 수 없는 상품 제외합니다.>")
+        continue
     
-    print(idx, "----> ", name.strip(), price.strip(), rate.strip(), rate_count.strip())
+    # 리뷰 100개 이상, 평점 4.5이상 되는 상품만 출력
+    if float(rate) >= 4.5 and int(rate_count) >= 100:
+        print(idx, "----> ", name.strip(), price.strip(), rate.strip(), rate_count.strip())
