b30c22d0fd70e3d828addfbc295d4333ae37b468
Walter Park <neodust223@gmail.com>
2024-04-04 23:07:59 +0900

    Update bs4_daummovie.py

--- webScraping_basic/bs4_daummovie.py
+++ webScraping_basic/bs4_daummovie.py
@@ -1,16 +1,20 @@
 import requests
 from bs4 import BeautifulSoup
 
-res = requests.get("https://search.daum.net/search?w=tot&q=2021%EB%85%84%EC%98%81%ED%99%94%EC%88%9C%EC%9C%84&DA=MOR&rtmaxcoll=MOR")
+res = requests.get("https://search.daum.net/search?w=tot&q=2023%EB%85%84%EC%98%81%ED%99%94%EC%88%9C%EC%9C%84&DA=MOR&rtmaxcoll=MOR")
 res.raise_for_status()
 soup = BeautifulSoup(res.text, "lxml")
 
-thumbs = soup.find_all("a", attrs={"class":"thumb_bf"})
+with open("bs_res", "w") as f:
+    f.write(soup.text)
 
-if len(thumbs) >= 0:
-    print("can't find image")
+# frame = soup.find("div", attrs={"class":"pdt2"})
+# if frame:
+#     thumbs = frame.find_all("a", attrs={"class":"thumb_bf"})
+# else:
+#     print("can't find frame")
 
-for thumb in thumbs:
-    image = thumb.find("img")
-    print("go")
-    print(image["src"])
+# for thumb in thumbs:
+#     print(thumbs)
+#     # image = thumb.find("img")
+#     # print(image["src"])
