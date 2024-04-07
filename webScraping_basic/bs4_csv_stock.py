c33a4ab31f2cbc5c1c15fc0505ffbc42f00b59fb
Walter Park <neodust223@gmail.com>
2024-04-07 19:03:34 +0900

    Update bs4_csv_stock.py
    csv로 저장하기 완료

--- webScraping_basic/bs4_csv_stock.py
+++ webScraping_basic/bs4_csv_stock.py
@@ -4,7 +4,15 @@ from bs4 import BeautifulSoup
 
 url = "https://finance.naver.com/sise/sise_market_sum.nhn?sosok=0&page="
 
-for page in range(1, 2):
+filename = "시가총액1-200.csv"
+f = open(filename, "w", encoding="utf-8-sig", newline="")
+writer = csv.writer(f)
+
+title = "N	종목명	현재가	전일비	등락률	액면가	시가총액	상장주식수	외국인비율	거래량	PER	ROE	토론실".split("\t")
+print(type(title))
+writer.writerow(title)
+
+for page in range(1, 5):
     res = requests.get(url + str(page))
     res.raise_for_status()
     soup = BeautifulSoup(res.text, "lxml")
@@ -16,6 +24,7 @@ for page in range(1, 2):
         # 공백열은 그냥 넘어간다.
         if len(columns) <= 1:
             continue
-        
+         
         data = [column.get_text().strip() for column in columns]
-        print(data)
+        # print(data)
+        writer.writerow(data)
