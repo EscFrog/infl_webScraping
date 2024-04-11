a9ec505cfd24a2f0f99aa5871e72a92e2838007b
Walter Park <neodust223@gmail.com>
2024-04-11 12:46:49 +0900

    Update selenium_test.py

--- webScraping_basic/selenium_test.py
+++ webScraping_basic/selenium_test.py
@@ -1,4 +1,18 @@
 from selenium import webdriver
 
 browser = webdriver.Chrome()
+
+# 네이버 이동
 browser.get("http://naver.com")
+
+# 2. 로그인 버튼 클릭
+elem = browser.find_element(y.CLASS, "link_login")
+elem.click()
+
+# 3. id, pw 입력
+browser.find_element(by.ID, "id").send_keys("naver_id")
+browser.find_element(by.ID, "pw").send_keys("password")
+
+# 4. 로그인 버튼 클릭
+browser.find_element(by.ID, "log.login").click()
+browser.find
